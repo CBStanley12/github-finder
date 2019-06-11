1629526be9874e753b8a07c0bf25d8f5377ebdca
Christopher Stanley <cbstanley12@gmail.com>
2019-06-10 18:18:56 -0600

    feat: search for users on GitHub; display profile information and 5 recent repositories

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github{
+    constructor() {
+        this.client_id = '33553b8200846fcc63fa';
+        this.client_secret = '8d599f50b1f0c4ccf2cfeb2859a25208219ca2da';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
